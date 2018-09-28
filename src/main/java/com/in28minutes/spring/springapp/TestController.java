d6d2fea5fc9be3839d78cf16d335305356a84bd6
Abhijit Samantaray <abhijit@simplilearn.com>
2018-09-28 15:42:57 +0530

    first code comit

---
+++ src/main/java/com/in28minutes/spring/springapp/TestController.java
@@ -0,0 +1,15 @@
+package com.in28minutes.spring.springapp;
+
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@GetMapping("/test")
+	public String test() {
+		return "tester";
+	}
+}
